57953d54bc150c03a74110dc4db3fe9f46461e91
kimHyeonJu <kimhyesom1202@gmail.com>
2026-01-01 21:33:57 +0900

    [section03] 2. 페이지 라우팅 설정하기

---
+++ section03/src/pages/search/index.tsx
@@ -0,0 +1,10 @@
+import { useRouter } from 'next/router';
+
+export default function Page() {
+  const router = useRouter();
+  const { q } = router.query; // 구조분해할당 또는 const q = router.query.q;
+
+  return (
+    <h1>Search 페이지의 쿼리 스트링 : { q }</h1>
+  )
+}
