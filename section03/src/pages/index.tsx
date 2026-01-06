af821835b72c16f266d6b8156b7a5c104fdfe12e
kimHyeonJu <kimhyesom1202@gmail.com>
2026-01-06 22:39:45 +0900

    [section03] 6. 스타일링

--- section03/src/pages/index.tsx
+++ section03/src/pages/index.tsx
@@ -1,7 +1,12 @@
+// CSS Module
+import styles from './index.module.css';
+
 export default function Home() {
   return (
-    <h1>
-      index 페이지
-    </h1>
+    <>
+      {/* <h1 style={{ color: 'red' }}> */}
+      <h1 className={styles.h1}>index 페이지</h1>
+      <h2 className={styles.h2}>H2 입니다.</h2>
+    </>
   )
 }
