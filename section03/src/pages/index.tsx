baa906165c4032b129619688d4a500723265bb6d
kimHyeonJu <kimhyesom1202@gmail.com>
2026-01-29 22:36:02 +0900

    [section03] 14. SSG 2. 정적 경로에 적용하기

--- section03/src/pages/index.tsx
+++ section03/src/pages/index.tsx
@@ -1,23 +1,18 @@
-// CSS Module
 import SearchableLayout from '@/components/searchable-layout';
 import styles from './index.module.css';
-// import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
-// import { useEffect } from 'react';
-import { InferGetServerSidePropsType } from 'next';
+import {  InferGetStaticPropsType } from 'next';
 import fetchBooks from '@/lib/fetch-books';
 import fetchRandomBooks from '@/lib/fetch-random-books';
 
-export const getServerSideProps = async () => {
-  // const allBooks = await fetchBooks();
-  // const recoBooks = await fetchRandomBooks();
-  // 참고) 요청이 먼저 모든 도서를 가져오고, 그 다음에 해당 도서를 가져오도록 순차적(직렬적인 방식)으로 동작한다.
+export const getStaticProps = async () => {
+  // SSG 방식
+  console.log('인덱스 페이지');
 
   const [allBooks, recoBooks] = await Promise.all([
     fetchBooks(),
     fetchRandomBooks(),
   ]);
-  // 참고) Promise.all 메서드 : 인수로 전달된 배열 안에 들어있는 모든 비동기 함수들을 동시에 실행시켜주는 메서드 → 병렬적인 방식으로 동작한다.
 
   return {
     props: {
@@ -27,7 +22,7 @@ export const getServerSideProps = async () => {
   };
 };
 
-export default function Home({ allBooks, recoBooks }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Home({ allBooks, recoBooks }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={styles.container}>
       <section>
