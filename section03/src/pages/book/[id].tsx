18a34a23c9dedec58aa9bcac88764f246537cd77
kimHyeonJu <kimhyesom1202@gmail.com>
2026-02-08 23:08:14 +0900

    [Fix] img 태그에 빠진 alt 추가

--- section03/src/pages/book/[id].tsx
+++ section03/src/pages/book/[id].tsx
@@ -32,7 +32,7 @@ export default function Page({ book } : InferGetServerSidePropsType<typeof getSe
         className={style.cover_img_container}
         style={{ backgroundImage: `url('${coverImgUrl}')` }}
       >
-        <img src={coverImgUrl} />
+        <img src={coverImgUrl} alt={title} />
       </div>
       <div className={style.title}>{title}</div>
       <div className={style.subTitle}>{subTitle}</div>
