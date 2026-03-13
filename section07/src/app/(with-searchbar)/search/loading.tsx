ba2d818cf75b55f93b38712612dd007fba596799
kimHyeonJu <kimhyesom1202@gmail.com>
2026-03-13 14:26:09 +0900

    [section07] 2. 페이지 스트리밍 적용하기

---
+++ section07/src/app/(with-searchbar)/search/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+  return <div>검색한 내용을 불러오는 중 입니다...</div>
+}
--- section07/src/app/(with-searchbar)/search/page.tsx
+++ section07/src/app/(with-searchbar)/search/page.tsx
@@ -1,7 +1,9 @@
 import BookItem from '@/components/book-item';
 import { BookData } from '@/types';
+import { delay } from '@/util/delay';
 
 export default async function Page({ searchParams }: { searchParams: Promise<{ q?: string }>; }) {
+  await delay(2000);
   const params = await searchParams;
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${params.q}`, { cache: 'force-cache'});
   if (!response.ok) {
