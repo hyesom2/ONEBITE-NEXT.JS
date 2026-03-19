37c379a0e5ad575716e36281326d73beb39111e8
kimHyeonJu <kimhyesom1202@gmail.com>
2026-03-19 17:08:26 +0900

    [section08] 6. 클라이언트 컴포넌트에서의 서버액션

--- section08/src/actions/create-review.action.ts
+++ section08/src/actions/create-review.action.ts
@@ -1,16 +1,17 @@
 "use server";
 
-import { revalidatePath, revalidateTag } from 'next/cache';
+import { revalidateTag } from 'next/cache';
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(_: any, formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
 
-  console.log(bookId, content, author);
-
   if (!bookId || !content || !author) {
-    return;
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 입력해주세요.",
+    };
   }
 
   try {
@@ -18,11 +19,20 @@ export async function createReviewAction(formData: FormData) {
       method: 'POST',
       body: JSON.stringify({ bookId, content, author }),
     });
-    console.log(response.status);
-    // revalidatePath(`/book/${bookId}`);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    };
+
     revalidateTag(`review-${bookId}`);
+    return {
+      status: true,
+      error: "",
+    };
   } catch (error) {
     console.error(error);
-    return;
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다, ${error}`,
+    };
   }
 }
