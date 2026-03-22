38849a32dd06ede3fb1318af93d421619fe25e99
kimHyeonJu <kimhyesom1202@gmail.com>
2026-03-22 21:14:56 +0900

    [section10] 1. 이미지 최적화

---
+++ section10/next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  logging: {
+    fetches: {
+      fullUrl: true
+    }
+  },
+  images: {
+    domains: [
+      'shopping-phinf.pstatic.net',
+    ]
+  }
+};
+
+export default nextConfig;
